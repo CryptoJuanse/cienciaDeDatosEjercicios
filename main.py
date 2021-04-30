7539be87bc58f04a23bba2d5d2e04ddb8cc8d12c
Sebastian Romero <jusromerov@correo.udistrital.edu.co>
2021-04-30 14:48:30 -0500

    Creada pagina para el formulario, solo falta agregar los filtros

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from sodapy import Socrata
-
 import pandas as pd
 
+#Obteniendo datos de vacunacion
 client = Socrata("www.datos.gov.co", None)
 results = client.get("sdvb-4x4j", limit=2000)
 results_df = pd.DataFrame.from_records(results)
 
 app = Flask(__name__)
-@app.route('/', methods = ['GET', 'POST'])
+#Pagina principal
+@app.route('/')
 def index():
     return render_template('index.html', tables=[results_df.to_html(classes='data', header=True)])
+
+#Pagina del formulario
+@app.route('/formulario')
+def show_formulario():
+    return render_template('formulario.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
