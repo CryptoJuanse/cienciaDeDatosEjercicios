abb30d753a99538abb68b8c23248a17e639b8579
Andrés David Becerra Dimaté <adbecerrad@correo.udistrital.edu.co>
2021-08-22 12:37:20 -0500

    Intento de gráfica con pyplot

--- talleres/pandasVisualizar/CERI_Vizualizacion_de_datos.py
+++ talleres/pandasVisualizar/CERI_Vizualizacion_de_datos.py
@@ -4,6 +4,18 @@ Spyder Editor
 This is a temporary script file.
 """
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import dateutil
 
 data = pd.read_csv('Colfuturo-Seleccionados.csv')
-print (data)
+#print (data)
+labels = data["Área"].unique()
+labels = np.sort(labels)
+S2 = data["Área"].value_counts().sort_index(ascending=True)
+
+#Gráfico de pie con porcentajes de áreas del datafame
+plt.pie(S2, labels=labels, autopct='%1.1f%%', shadow=True, startangle=140)
+
+###plt.legend()
+plt.show()
