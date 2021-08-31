8c6304e76dbd40a323ecae2302a7d24b308b49d7
Sebastian Romero <jusromerov@correo.udistrital.edu.co>
2021-08-31 08:31:24 -0500

    Actualizacion del taller de Visualizacion

--- talleres/pandasVisualizar/CERI_Vizualizacion_de_datos.py
+++ talleres/pandasVisualizar/CERI_Vizualizacion_de_datos.py
@@ -6,4 +6,9 @@ This is a temporary script file.
 import pandas as pd
 
 data = pd.read_csv('MovilizacionSalienteCERI.csv')
-print (data)
+print (data.columns)
+
+#Limpieza de datos NAN
+data['Presupuesto asignado'] = data['Presupuesto asignado'].fillna(0)
+
+#Grafica plot
